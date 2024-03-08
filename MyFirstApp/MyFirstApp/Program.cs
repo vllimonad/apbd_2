fba4828941bf98b0c27afd7cb9053897da248ea2
Uladzislau Klunduk <klundukvlad40@gmail.com>
2024-03-08 18:48:41 +0100

    add method getAverage

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -5,3 +5,14 @@ Console.WriteLine("Hello, World!");
 //modification 1
 //modification 2
 //modification 3
+static double GetAverage(int[] numbers)
+{
+    double sum = 0.0;
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
+}
+
